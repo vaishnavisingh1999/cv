cc6eaaca28c58b197d7121a0c9fb6457d5ca925d
vaishnavisingh1999 <121811896+vaishnavisingh1999@users.noreply.github.com>
2023-03-17 13:48:07 +0530

    colorpalette

---
+++ script.js
@@ -0,0 +1,34 @@
+const container=document.querySelector(" .container");
+const refreshBtn=document.querySelector(" .refresh-btn");
+
+
+const maxPaletteBoxes =32;
+const generatePalette =() =>{
+    container.innerHTML="";//clearing the container
+    for (let i=0; i<maxPaletteBoxes; i++){
+        //generating a random hex color
+        let randomHex=Math.floor(Math.random() *0xffffff).toString(16);
+    randomHex = `#${randomHex.padStart(6,"0")}`;
+
+    //creating a new  li element and inserting it to the container
+
+    const color = document.createElement("li");
+    color.classList.add("color");
+    color.innerHTML =`<div class="rect-box" style ="background: ${randomHex}"></div>
+                             <span class="hex-value">${randomHex}</span>`;
+    //adding click event to current li element to copy the color
+    color.addEventListener("click",() => copyColor(color,randomHex));
+    container.appendChild(color);
+    }
+    
+}
+generatePalette();
+const copyColor =(elem,hexVal) =>{
+    const colorElement= elem.querySelector(".hex-value");
+    navigator.clipboard.writeText(hexVal).then(()=>{
+        colorElement.innerText="Copied";
+    })
+}
+
+
+refreshBtn.addEventListener("click" ,generatePalette);
